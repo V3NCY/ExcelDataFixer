d03cfde23bef082db65573aa457fe0746f0ba01b
V3NCY <venceslavam@gmail.com>
2025-01-24 17:37:29 +0200

    updates

--- obj/Debug/net8.0/ExcelDataFixer.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExcelDataFixer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExcelDataFixer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1824ad075aec98cbb9019cf8305d1cb02f1b96ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73cd55b3164d82b0a64c748eef11629815dcc87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExcelDataFixer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExcelDataFixer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
